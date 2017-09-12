230a5ff0b428de1b6a588ff6c54fd7451f283341
Mukhammad Karimov <karimov8887@gmail.com>
2017-09-12 09:42:39 +0900

    DNA translation

--- rna-transcription/rna_transcription.py
+++ rna-transcription/rna_transcription.py
@@ -1,2 +1,19 @@
-def to_rna():
-    pass
+def to_rna(strand):
+    # default rna strand
+    rna_strand = ''
+
+    # loop over and check each character
+    for character in strand:
+        if character == 'A':
+            rna_strand += 'U'
+        elif character == 'T':
+            rna_strand += 'A'
+        elif character == 'C':
+            rna_strand += 'G'
+        elif character == 'G':
+            rna_strand += 'C'
+        else:
+            return ''
+            
+    return rna_strand 
+            
